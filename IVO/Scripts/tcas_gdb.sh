e61bf9d4de969bde34cd95e8f2430cf234d8198e
Borbon45 <eastey123@hotmail.co.uk>
2026-05-18 15:07:12 +0100

    Update tcas_gdb.sh
    annotation

--- IVO/Scripts/tcas_gdb.sh
+++ IVO/Scripts/tcas_gdb.sh
@@ -6,10 +6,22 @@
 #SBATCH -J Tcas
 #SBATCH -t 48:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=qxf20ddu@uea.ac.uk
+#SBATCH --mail-user=
 #SBATCH --mem=80000
 #SBATCH -c 8
 
+# ==============================================================================
+# SECTION 1: SYSTEM ENVIRONMENT & HPC MODULE LOADING
+# ==============================================================================
+
+# Load SIFT4G framework (v2.0.0) into the shell environment from the cluster
 module add SIFT4G/2.0.0
 
-make-SIFT-db-all.pl -config /gpfs/home/qxf20ddu/SIFT4G_tests/SIFT4G_Create_Genomic_DB/2_test/tribolium_castaneum_config.txt
+# ==============================================================================
+# SECTION 2: CUSTOM REFERENCE DATABASE COMPILATION
+# ==============================================================================
+
+# Execute the core Perl automation wrapper to compile the genomic SIFT database
+make-SIFT-db-all.pl \
+  # Configuration Profile: Path to the structural text file mapping the genome's paths
+  -config /gpfs/home/qxf20ddu/SIFT4G_tests/SIFT4G_Create_Genomic_DB/2_test/tribolium_castaneum_config.txt
